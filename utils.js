325838d1b06a0fe0095855af5e897b6647cc58b2
elvin <ali.elvin@gmail.com>
2015-08-30 23:00:06 +0100

    initial commit

---
+++ utils.js
@@ -0,0 +1,44 @@
+/*jslint node: true */
+'use strict';
+
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+var path = require('path');
+var urlParse = require('url').parse;
+var slug = require('slug');
+var rimraf = require('rimraf');
+
+module.exports.saveFile = function saveFile(filename, contents, callback) {
+    mkdirp(path.dirname(filename), function(err) {
+        if (err) {
+            return callback(err);
+        }
+        fs.writeFile(filename, contents, callback);
+    });
+};
+
+// remove temp directory 
+module.exports.cleanUp = function cleanUp(dirname, callback) {
+    rimraf(dirname, function(err) {
+        if (err) {
+            return callback(err);
+        }
+    });
+};
+
+module.exports.urlToFilename = function urlToFilename(url) {
+    var parsedUrl = urlParse(url);
+    var urlPath = parsedUrl.path.split('/')
+        .filter(function(component) {
+            return !!component;
+        })
+        .map(function(component) {
+            return slug(component);
+        })
+        .join('/');
+    var filename = path.join(parsedUrl.hostname, urlPath);
+    if (!path.extname(filename).match(/htm/)) {
+        filename += '.html';
+    }
+    return filename;
+};
