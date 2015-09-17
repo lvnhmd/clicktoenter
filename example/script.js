e0a250d09c0828a7ade44927df1a8f4b61d2ff18
elvin <ali.elvin@gmail.com>
2015-09-17 17:20:52 +0100

    add example

---
+++ example/script.js
@@ -0,0 +1,11 @@
+(function () {
+  var skills = DB.skills, developers = DB.developers;
+
+  var app = new Ractive({
+    el: '#root',
+    template: '#tpl-app',
+    data: {
+      skills: _.keys(DB.skills)
+    }
+  });
+}());
