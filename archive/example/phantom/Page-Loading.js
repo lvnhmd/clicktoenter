85156118b99e67147700906a81f12b880e98224b
elvin <ali.elvin@gmail.com>
2015-10-07 13:52:04 +0100

    remove old files

--- archive/example/phantom/Page-Loading.js
+++
@@ -1,8 +0,0 @@
-var page = require('webpage').create();
-page.open('http://www.elvinali.co.uk', function(status) {
-  console.log("Status: " + status);
-  if(status === "success") {
-    page.render('elvin.png');
-  }
-  phantom.exit();
-});
